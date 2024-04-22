5f29ac12b6eb314208630a0ad0740e794a0e8724
Austin Colt <austin@jcolt.net>
2024-04-22 16:42:09 -0600

    Complete Prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        while (true)
+        {
+            int currentGrade = GetGrade();
+            string currentLetterGrade = GetLetterGrade(currentGrade);
+            string prefix = GetPrefix(currentGrade);
+
+            Console.WriteLine($"\nYour grade is a/an {currentLetterGrade}{prefix}.");
+        }
+        
+    }
+
+    static int GetGrade()
+    {
+        Console.Write("What is your grade as a percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        return grade;
+    }
+
+    static string GetLetterGrade(int grade)
+    {
+        if (grade >= 90)
+        {
+            return "A";
+        }
+        else if (grade >= 80)
+        {
+            return "B";
+        }
+        else if (grade >= 70)
+        {
+            return "C";
+        }
+        else if (grade >= 60)
+        {
+            return "D";
+        }
+        else
+        {
+            return "F";
+        }
+    }
+
+    static string GetPrefix(int grade)
+    {
+        int lastDigit = grade % 10;
+
+        if (lastDigit >= 7 && grade < 97 && grade >= 60)
+        {
+            return "+";
+        }
+        else if (lastDigit < 3 && grade < 97 && grade >= 60)
+        {
+            return "-";
+        }
+        else
+        {
+            return "";
+        }
     }
 }
