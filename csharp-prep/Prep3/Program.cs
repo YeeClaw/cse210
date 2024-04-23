1c6d60a0643da1a59b084abb535fa0cb89c0a0a1
Austin Colt <68457568+YeeClaw@users.noreply.github.com>
2024-04-22 23:03:20 -0600

    Add complete guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the Guessing Game!");
+        Console.WriteLine("Your goal is to guess the magic number between 1 and 100.");
+
+        Console.WriteLine("Would you like to play? (yes/no)");
+        string play = Console.ReadLine();
+
+        while (play == "yes")
+        {
+            int numGuesses = RunGuessGame();
+            Console.WriteLine($"It took you {numGuesses} guesses to find the magic number.");
+
+            Console.WriteLine("\nWould you like to play again? (yes/no)");
+            play = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
+    }
+
+    static int RunGuessGame()
+    { 
+        Random rnd = new();
+        int magicNumber = rnd.Next(1, 101);
+        // The `Next()` method takes advantage of a mechanic called method overloading. In this way, a method can
+        // have multiple definitions, each with a different set of parameters.
+        int guess;
+        int attempts = 0;
+
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+
+            attempts++;
+        } while (guess != magicNumber);
+
+        return attempts;
     }
 }
